30583a28934b9ffa0e8515597b9e3e62dbf4e862
Thor Even Tutturen <thor@bustbyte.no>
2017-03-17 09:12:58 +0100

    rename zumo_border to ino

--- Zumo_border.ino
+++ Zumo_border.ino
@@ -1,5 +1,5 @@
 
-//Ser om roboten er på vei ut av sirkel, og snur roboten rundt nesten 180 grader. 
+//Ser om roboten er på vei ut av sirkel, og snur roboten rundt nesten 180 grader.
 
 void detectLine(){
 
@@ -11,29 +11,29 @@ Drive forward and turn left or right when border is detected
 #include <Pushbutton.h>
 #include <QTRSensors.h>
 #include <ZumoReflectanceSensorArray.h>
- 
+
 #define LED 13
- 
+
 // this might need to be tuned for different lighting conditions, surfaces, etc.
-#define QTR_THRESHOLD  1800 // 
-  
+#define QTR_THRESHOLD  1800 //
+
 // these might need to be tuned for different motor types
 #define REVERSE_SPEED     200 // 0 is stopped, 400 is full speed
 #define TURN_SPEED        400
 #define FORWARD_SPEED     100
 #define REVERSE_DURATION  300 // ms
 #define TURN_DURATION     400 // ms
- 
+
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
- 
+
 #define NUM_SENSORS 6
 unsigned int sensor_values[NUM_SENSORS];
- 
+
 ZumoReflectanceSensorArray sensors;
 
 
- 
+
 void setup()
 {
    sensors.init();
